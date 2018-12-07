e11de6cfc341c2aea33442e6f9d879416dd4dfa2
Gel Studio <devondaviau@yahoo.ca>
2018-12-07 13:20:31 -0500

    add nprogress to header and create _document to fix styled components fouc

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,7 +4,17 @@ import styled from 'styled-components';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
 
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+} 
 
 const Logo = styled.h1`
   font-size: 4rem;
