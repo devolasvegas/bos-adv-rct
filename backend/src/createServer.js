71d680081a2ceee208f4e2f71af90d5062014a8c
Gel Studio <devondaviau@yahoo.ca>
2018-12-07 16:33:23 -0500

    make some tweaks so server runs

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,8 +1,8 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const Mutation = require('Mutation');
-const Query = require('Query');
-const db = require('db');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
 
 // Create the GraphQL Yoga Server
 
