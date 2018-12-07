d94b4212b4f41d7956fb4ea963d37d1903b86179
Gel Studio <devondaviau@yahoo.ca>
2018-12-07 11:39:52 -0500

    import next router and nprogress

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,10 @@
 import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+
 
 const Logo = styled.h1`
   font-size: 4rem;
