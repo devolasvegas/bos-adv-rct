d5ebc5064e4400fbc37f25c9187523c1ffb03db4
Gel Studio <devondaviau@yahoo.ca>
2018-12-11 15:55:58 -0500

    fix bug caused by prisma.yml and re-deploy data model

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,8 @@
 const Query = {
-  dogs(parent, args, cts, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  },
+  // dogs(parent, args, cts, info) {
+  //   global.dogs = global.dogs || [];
+  //   return global.dogs;
+  // },
 };
 
 module.exports = Query;
