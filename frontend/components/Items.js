9488c9398d093647118d26d218769a50f78b4ce0
Gel Studio <devondaviau@yahoo.ca>
2018-12-13 16:45:26 -0500

    update Items component, and create Item component

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+
+import Item from './Item';
 
 const  ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,19 +18,34 @@ const  ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${ props => props.theme.maxWidth };
+  margin: 0 auto;
+`;
+
 class Items extends Component {
   render() {
     return (
-      <div>
-        <p>Items!</p>
+      <Center>
           <Query query={ ALL_ITEMS_QUERY }>
             { ({ data, error, loading }) => {
+              if(loading) return <p>Loading &hellip;</p>;
+              if(error) return <p>Error: { error.message }</p>;
               return (
-                <p>I found {data.items.length} items.</p>
+                <ItemsList>
+                  { data.items.map(item => <Item key={ item.id } item={ item } />)}
+                </ItemsList>
               )
             }}
           </Query>
-      </div>
+      </Center>
     );
   }
 }
