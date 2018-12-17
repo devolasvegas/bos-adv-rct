d135b35efe8a771d9641833d0978bbfd0a5e3e15
devolasvegas <devondaviau@yahoo.ca>
2018-12-17 13:13:22 -0500

    wrap mutation query and form in query component

--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Mutation } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Router from 'next/router';
 
@@ -51,65 +51,72 @@ class UpdateItem extends Component {
 
   render() {
     return (
-      <Mutation mutation={ UPDATE_ITEM_MUTATION } variables={ this.state }>
-        { (UpdateItem, { loading, error }) => (
-          <Form onSubmit={
-            async e => {
-              // Stop the form from submitting
-              e.preventDefault();
-              // Call the mutation
-              const res = await UpdateItem();
-              // Send user to single item page
-              console.log(res);
-              Router.push({
-                pathname: '/item',
-                query: { id: res.data.UpdateItem.id },
-              })
-            }
-          }>
-            <Error error={ error } />
-            <fieldset disabled={ loading } aria-busy={ loading }>
-              <label htmlFor="title">
-                Title
-                <input 
-                  type="text" 
-                  id="title" 
-                  name="title" 
-                  placeholder="Title" 
-                  value={ this.state.title }
-                  onChange={ this.handleChange }
-                  required 
-                />
-              </label>
-              <label htmlFor="price">
-                Price
-                <input 
-                  type="number" 
-                  id="price" 
-                  name="price" 
-                  placeholder="Price" 
-                  value={ this.state.price }
-                  onChange={ this.handleChange }
-                  required 
-                />
-              </label>
-              <label htmlFor="description">
-                Description
-                <textarea 
-                  id="description" 
-                  name="description" 
-                  placeholder="Enter a Description" 
-                  value={ this.state.description }
-                  onChange={ this.handleChange }
-                  required 
-                />
-              </label>
-              <button type="submit">Submit</button>
-            </fieldset>
-          </Form>
-          )
-        }
-      </Mutation>
+      <Query query={ SINGLE_ITEM_QUERY } variables={{ id: this.props.id }}>
+        { ({data, loading}) => {
+          return (
+            
+        <Mutation mutation={ UPDATE_ITEM_MUTATION } variables={ this.state }>
+          { (UpdateItem, { loading, error }) => (
+            <Form onSubmit={
+              async e => {
+                // Stop the form from submitting
+                e.preventDefault();
+                // Call the mutation
+                const res = await UpdateItem();
+                // Send user to single item page
+                console.log(res);
+                Router.push({
+                  pathname: '/item',
+                  query: { id: res.data.UpdateItem.id },
+                })
+              }
+            }>
+              <Error error={ error } />
+              <fieldset disabled={ loading } aria-busy={ loading }>
+                <label htmlFor="title">
+                  Title
+                  <input 
+                    type="text" 
+                    id="title" 
+                    name="title" 
+                    placeholder="Title" 
+                    value={ this.state.title }
+                    onChange={ this.handleChange }
+                    required 
+                  />
+                </label>
+                <label htmlFor="price">
+                  Price
+                  <input 
+                    type="number" 
+                    id="price" 
+                    name="price" 
+                    placeholder="Price" 
+                    value={ this.state.price }
+                    onChange={ this.handleChange }
+                    required 
+                    />
+                </label>
+                <label htmlFor="description">
+                  Description
+                  <textarea 
+                    id="description" 
+                    name="description" 
+                    placeholder="Enter a Description" 
+                    value={ this.state.description }
+                    onChange={ this.handleChange }
+                    required 
+                  />
+                </label>
+                <button type="submit">Submit</button>
+              </fieldset>
+            </Form>
+            )
+          }
+        </Mutation>
+          );
+        } }
+      </Query>
     )
   }
 }
