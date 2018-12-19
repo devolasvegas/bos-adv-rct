fed20be838882167899c512ea23d6baf10b785fe
Gel Studio <devondaviau@yahoo.ca>
2018-12-19 11:29:34 -0500

    add query component and error handling into singleitem component

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import styled from 'styled-components';
+
+import Error from './ErrorMessage';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      largeImage
+    }
+  }
+`;
 
 class SingleItem extends Component {
   render() {
     return (
-      <div>
-        <p>Single Item Component, biiiiiiittttttccchhhhh!!!!</p>
-      </div>
+      <Query 
+        query={ SINGLE_ITEM_QUERY } 
+        variables={{ id: this.props.id }}
+      >
+        { ({ error, loading, data }) => {
+          if(error) return <Error error={ error } />;
+          if(loading) return <p>Loading &hellip;</p>;
+          if(!data.item) return <p>No item found for ID: { this.props.id }</p>;
+          console.log(data)
+          return <p>Single Item Component, biiiiiiittttttccchhhhh!!!!</p>;
+        } }
+      </Query>
     )
   }
 }
