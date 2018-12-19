bbcfdb0846dd58d7455f822ea899fc92f7efccf4
Gel Studio <devondaviau@yahoo.ca>
2018-12-19 11:50:46 -0500

    more styles

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 import Error from './ErrorMessage';
 
@@ -13,6 +14,15 @@ const SingleItemStyles = styled.div`
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
   min-height: 800px;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
@@ -40,7 +50,14 @@ class SingleItem extends Component {
           const item = data.item;
           return (
             <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | { item.title }</title>
+              </Head>
               <img src={ item.largeImage } alt={ item.title }/>
+              <div className="details">
+                <h2>Viewing { item.title }</h2>
+                <p>{ item.description }</p>
+              </div>
             </SingleItemStyles>
           );
         } }
