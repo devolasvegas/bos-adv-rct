f3c954d614d19ba477c1d09221117e17a0cbd58e
devolasvegas <devondaviau@yahoo.ca>
2019-01-02 22:54:03 -0500

    create pagination component

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+const Pagination = props => (
+  <PaginationStyles>
+
+  </PaginationStyles>
+);
+
+export default Pagination;
