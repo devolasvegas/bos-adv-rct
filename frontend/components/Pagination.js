b9e8186c994b550706a3db38b08492c811fcabb6
devolasvegas <devondaviau@yahoo.ca>
2019-01-02 23:12:18 -0500

    add in query component and pass it our query

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import gql from 'graphql-tools';
-import Query from 'graphql-tools';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 import PaginationStyles from './styles/PaginationStyles';
 
@@ -16,7 +16,11 @@ const PAGINATION_QUERY = gql`
 
 const Pagination = props => (
   <PaginationStyles>
-    <p>Sup, I'm the pagination.</p>
+    <Query query={ PAGINATION_QUERY }>
+      { ({ data, loading, error }) => (
+        <p>Sup, I'm the pagination. { data.itemsConnection.aggregate.count }</p>
+      ) }
+    </Query>
   </PaginationStyles>
 );
 
