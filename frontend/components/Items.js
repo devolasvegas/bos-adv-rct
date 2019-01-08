eecc069881afe815e66b7ebe5315c6613aa33a2e
devolasvegas <devondaviau@yahoo.ca>
2019-01-07 22:02:40 -0500

    import perPage value from config

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 
 import Item from './Item';
 import Pagination from './Pagination';
+import perPage from '../config';
 
 const  ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
