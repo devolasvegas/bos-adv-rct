c884892027dac83d208adf3b62c84f8e4fa4136e
devolasvegas <devondaviau@yahoo.ca>
2019-01-07 22:12:57 -0500

    pass values to query for pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -37,7 +37,9 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={ this.props.page } />
-          <Query query={ ALL_ITEMS_QUERY }>
+          <Query query={ ALL_ITEMS_QUERY } variables={{
+            skip: this.props.page * perPage - perPage,
+          }}>
             { ({ data, error, loading }) => {
               if(loading) return <p>Loading &hellip;</p>;
               if(error) return <p>Error: { error.message }</p>;
