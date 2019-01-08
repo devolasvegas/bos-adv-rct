ac72dade39c7516dea5a16586c0572aecc9097fc
devolasvegas <devondaviau@yahoo.ca>
2019-01-07 22:05:36 -0500

    add arguments to query

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -8,8 +8,8 @@ import Pagination from './Pagination';
 import perPage from '../config';
 
 const  ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${ perPage }) {
+    items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
       id
       title
       price
