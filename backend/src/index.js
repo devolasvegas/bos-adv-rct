6b1142460cc414f176a0f49b200a52add7f20baa
devolasvegas <devondaviau@yahoo.ca>
2019-01-08 22:23:53 -0500

    add in cookieParser middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,9 @@ const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
+// Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO Use express middleware to populate current user
 
 server.start({
