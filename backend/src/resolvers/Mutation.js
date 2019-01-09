5e2ea3c0e64c494830da3832d491ca975ca2de26
devolasvegas <devondaviau@yahoo.ca>
2019-01-08 22:47:16 -0500

    create signup mutation resolver to create new user, hash their pw, and create a cookie with a jwt

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -34,6 +37,32 @@ const Mutations = {
     // TODO
     // 3. Delete the item
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // Make sure the email is lowercase
+    args.email = args.email.toLowerCase();
+    // Hash the password
+    const password = await bcrypt.hash(args.password, 10);
+    // Create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        }
+      },
+      info
+    );
+    // create the JWT for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+    });
+    // Return the user to the browser
+    return user;
   },
 };
 
