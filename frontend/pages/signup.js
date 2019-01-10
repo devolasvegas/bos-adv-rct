d2abbd37e3192560dc19b45ac558fe28e8c85b9e
devolasvegas <devondaviau@yahoo.ca>
2019-01-09 22:07:21 -0500

    add extra signup components and put in styled grid

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,9 +1,19 @@
-import Signup from '../components/Signup'
+import styled from 'styled-components';
+
+import Signup from '../components/Signup';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const SignupPage = () => (
-  <div>
+  <Columns>
     <Signup />
-  </div>
+    <Signup />
+    <Signup />
+  </Columns>
 )
 
 
