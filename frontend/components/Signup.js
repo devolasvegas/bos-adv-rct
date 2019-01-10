88208ea5356cb2433d7ed078270e075d3f35c418
devolasvegas <devondaviau@yahoo.ca>
2019-01-09 21:59:53 -0500

    imports

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import Form from './styles/Form';
+import Error from './styles/Error';
 
 class Signup extends Component {
   render() {
