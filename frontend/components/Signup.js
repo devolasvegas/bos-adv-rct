b165625de756e06ae4d5115c8610f09810a778b9
devolasvegas <devondaviau@yahoo.ca>
2019-01-09 22:15:21 -0500

    add in form fields

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -10,7 +10,36 @@ class Signup extends Component {
     return (
       <Form>
         <fieldset>
-          <h2>Sign Up for an Account</h2>
+          <label htmlFor="email">
+            Email
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="text"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.saveToState}
+            />
+          </label>
         </fieldset>
       </Form>
     )
