45cbe7d2f09d8ccfad7e89e259fa94c16894bd6f
devolasvegas <devondaviau@yahoo.ca>
2019-01-29 21:16:40 -0500

    add signout mutation resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -84,7 +84,11 @@ const Mutations = {
     })
     // 5. Return the user
     return user;
-  }
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'goodbye' };
+  },
 };
 
 module.exports = Mutations;
