96d5b43433257f5a7bdb7a86e7104b79cb88a05b
devolasvegas <devondaviau@yahoo.ca>
2019-01-29 21:28:48 -0500

    create mutation and wire up mutation component

--- frontend/components/Signout.js
+++ frontend/components/Signout.js
@@ -3,8 +3,21 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
 
+const SIGN_OUT_MUTATION = gql`
+  mutation SIGN_OUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`
+
 const Signout = props => (
-  <button>Sign Out</button>
+  <Mutation
+    mutation={SIGN_OUT_MUTATION}
+    refetchQueries={[{ CURRENT_USER_QUERY }]}
+  >
+    {signout => <button onClick={signout}>Sign Out</button>}
+  </Mutation>
 )
 
 export default Signout
